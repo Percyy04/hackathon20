c2143cc4e8c19894da960c9d3f36aa5089841190
BuiDiepAnh <buidiepanh0905@gmail.com>
2025-11-29 16:08:58 +0700

    base set up

--- crowdspark-be/server.js
+++ crowdspark-be/server.js
@@ -1,22 +1,25 @@
-require('dotenv').config();
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const cors = require('cors');
-const socketHandler = require('./src/socket/socketHandler');
+const { ENV } = require("./src/lib/env.js");
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const connectMongoDB = require("./src/store/mongo.js");
+//const socketHandler = require("./src/socket/socketHandler");
 
 const app = express();
 app.use(cors());
 
+connectMongoDB();
+
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "*", methods: ["GET", "POST"] }
+  cors: { origin: "*", methods: ["GET", "POST"] },
 });
 
 // Khởi động luồng Socket
-socketHandler(io);
+//socketHandler(io);
 
-const PORT = process.env.PORT || 3000;
+const PORT = ENV.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`🚀 Server chạy tại http://localhost:${PORT}`);
 });
